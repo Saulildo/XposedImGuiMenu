9460504a0c6f8ac190a94b7b1f45c44433bd99a2
Saulildo <104403007+Saulildo@users.noreply.github.com>
2025-10-02 18:03:05 -0300

    Update Menu.h

--- app/src/main/cpp/Menu.h
+++ app/src/main/cpp/Menu.h
@@ -15,13 +15,49 @@ void SetupImGui() {
 
 void DrawMenu() {
     static bool showMenu = true;
-    SetNextWindowSize(ImVec2(400, 400), ImGuiCond_FirstUseEver);
-    if (Begin("Menu", &showMenu)) {
-        Text("Hello, world!");
-        if (Button("Close")) {
+    SetNextWindowSize(ImVec2(400, 500), ImGuiCond_FirstUseEver);
+    if (Begin("Xposed Menu - com.herogame.gplay.survival.lite", &showMenu)) {
+        
+        // ESP Section
+        if (CollapsingHeader("ESP Settings")) {
+            Checkbox("Enable ESP", &g_ESPConfig.Draw);
+            
+            if (g_ESPConfig.Draw) {
+                Indent(20.0f);
+                Checkbox("Show Lines", &g_ESPConfig.Line);
+                Checkbox("Show HP", &g_ESPConfig.Hp);
+                Checkbox("Show Distance", &g_ESPConfig.Distance);
+                Checkbox("Show Bones", &g_ESPConfig.Bones);
+                Checkbox("Show Name", &g_ESPConfig.Name);
+                Checkbox("Show 3D Box", &g_ESPConfig.Box3D);
+                
+                Separator();
+                Text("Box Adjustments:");
+                SliderFloat("Top Position", &g_topPosFloat, 1.0f, 3.0f);
+                SliderFloat("Bottom Position", &g_botPosFloat, 0.0f, 1.0f);
+                SliderFloat("Distance Scale", &g_clappedFloat, 50.0f, 200.0f);
+                SliderFloat("Box Scale", &g_calculatedPositionFloat, 1.0f, 5.0f);
+                
+                Separator();
+                Text("Bone Color:");
+                ColorEdit4("Bones", g_BonesColor);
+                
+                Unindent(20.0f);
+            }
+        }
+        
+        Separator();
+        
+        // Other features section (placeholder)
+        if (CollapsingHeader("Other Features")) {
+            Checkbox("Example feature", &some_feature);
+        }
+        
+        Separator();
+        
+        if (Button("Close Menu")) {
             showMenu = false;
         }
-        Checkbox("Change something", &some_feature);
     }
     End();
 }
